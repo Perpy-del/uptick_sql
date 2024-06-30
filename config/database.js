ab818df448a3438b613f33bb7369b99ebaab6740
Perpy-del <perpetualmeninwa@gmail.com>
2024-06-30 09:38:18 +0100

    fix: :bug: remote db connection fix

--- config/database.js
+++ config/database.js
@@ -3,6 +3,8 @@ const { Sequelize } = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require('.');
 
-const sequelize = new Sequelize(config[env]);
+// const sequelize = new Sequelize(config[env]);
+
+const sequelize = new Sequelize(process.env.PROD_DB_HOST)
 
 module.exports = sequelize;
